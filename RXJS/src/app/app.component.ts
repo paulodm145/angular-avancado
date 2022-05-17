650b33f9688b92b6f6d5bdaa931985ef0bdca902
Paulo R. Bolsanello <paulo.bolsanello@basis.com.br>
2022-05-17 08:43:13 -0300

    Início do estudo sobre observables e promisses. Anotações em Aruivo Anexo

--- RXJS/src/app/app.component.ts
+++ RXJS/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
+  
   title = 'RXJS';
+
+  minhaPromisse(nome: string) : Promise<string>{
+    return new Promise((resolve, reject) => {
+      if (nome === 'Paulo') {
+        setTimeout(() => {
+          resolve('Seja bem vindo ' + nome);
+        }, 3000);
+      } else {
+        reject('Ops! Você não é o Paulo !');
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.minhaPromisse('Paulo').then(
+      result => console.log(result)
+    )
+
+    this.minhaPromisse('Jose').then(
+      result => console.log(result)
+    ).catch(error => console.log('Você não é o Paulo'))
+  }
+
 }
