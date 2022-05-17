10e9396c1dafdb6f282059fb5ec145536c969959
Paulo R. Bolsanello <paulo.bolsanello@basis.com.br>
2022-05-17 10:28:29 -0300

    Criar uma observable

---
+++ Anotações/Promisses_Observables.md
@@ -0,0 +1,9 @@
+promise retornar uma reposta em caso de sucesso ou erro. È um Callback simples, trás o resultado ou falha, promessa de um resultado 
+
+Observable: mais completo, reativo, é um promisse que pode ser convertido em promisse, retornar dados, a observable continua a entrega os dados , permanecendo viva a cada entrega. 
+
+Observable pode ser cancelado. Retry: nova tentativa de execução da Observable após o erro. Muito mais complexo. Push e pull(enviar e receber). 
+
+
+ng new RXJS --minimal=true
+
--- RXJS/src/app/app.component.ts
+++ RXJS/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,8 +19,24 @@ export class AppComponent implements OnInit {
           resolve('Seja bem vindo ' + nome);
         }, 3000);
       } else {
-        reject('Ops! Você não é o Paulo !');
+        reject('Promisse: Ops! Você não é o Paulo !');
       }
+    })
+  }
+
+    /** MINHA PRIMEIRA OBSERVABLE */
+  /** Enquanto houver informações */
+  minhaObservable(nome: string) : Observable<string> {
+    return new Observable(subscriber => {
+        if(nome === 'Paulo') {
+          subscriber.next('Olá Mundo!'); /** Callback para o subscriber receber a informação */
+          subscriber.next('Olá Mundo! Mais uma vez ');
+          setTimeout(() => {
+            subscriber.next('Respostas com Delay - observable');
+          }, 5000);
+        } else {
+          subscriber.error('Ops! Deu erro!');
+        }
     })
   }
 
@@ -30,7 +47,14 @@ export class AppComponent implements OnInit {
 
     this.minhaPromisse('Jose').then(
       result => console.log(result)
-    ).catch(error => console.log('Você não é o Paulo'))
+    ).catch(error => console.log('Promisse 2: Você não é o Paulo'))
+
+    /** Primeira Instrução Lógica trás o sucesso e a segunda trás o erro */
+    this.minhaObservable('Paulo').subscribe(result => console.log(result), erro => console.log(erro))
   }
+
+
+
+ 
 
 }
